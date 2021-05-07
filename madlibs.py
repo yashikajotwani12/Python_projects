7ddbc3e19c1525cd1cf5df18234ea53eb5058170
Yashika <yashikajotwani12@gmail.com>
2021-05-07 17:17:52 +0530

    done

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#string concatanation
+# youthuber="Yashika Jotwani"
+# print("Subscribe to "+youthuber)
+# print("Subscribe to {}".format(youthuber))
+# print(f"Subscribe to {youthuber}")
+
+
+adj = input("Adjective: ")
+verb1= input("Verb: ")
+verb2= input("Verb: ")
+famous_person= input("Famous person: ")
+madlib=f"Computer Programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
---
+++ rock_paper_scissors.py
@@ -0,0 +1,6 @@
+import random
+
+def play():
+    user=input("' r ' for rock, ' p' for paper, ' s ' for scissors")
+    computer = random.choice(['r','p','s'])
+     
